16a3b84157b5696fc2e543c4d91e7a3e3a6e867f
Abhishek Das <abhishek.dasmca@outlook.com>
2020-09-22 23:04:30 +0530

    Repo added

---
+++ BusinessLogic/Repositories/IRepositories.cs
@@ -0,0 +1,12 @@
+﻿using BusinessLogic.Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessLogic.Repositories
+{
+    public interface IRepositories
+    {
+        Membership findByProduct(String product);
+    }
+}
