302f016635e73f8b93d68288b6c968a8e10d1f5f
Abhishek Das <abhishek.dasmca@outlook.com>
2020-09-22 22:59:43 +0530

    Services Added

---
+++ BusinessLogic/Service/IShippingServices.cs
@@ -0,0 +1,12 @@
+﻿using BusinessLogic.Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessLogic.Service
+{
+    public interface IShippingServices
+    {
+        PackingSlip generatePackingSlip(Order order);
+    }
+}
